0949b7b5b9752375e7a0e01b84dcf662ffe21295
Jacob L <lecoqjacob@gmail.com>
2025-05-28 14:39:11 -0500

    chore: fallback to deffault blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,21 +1,25 @@
-return {
-  "saghen/blink.cmp",
-  build = "cargo +nightly build --release",
-  ---@class PluginLspOpts
-  opts = {
-    fuzzy = { implementation = "prefer_rust" },
-    signature = { enabled = true },
-    keymap = {
-      preset = "super-tab",
-    },
-    sources = {
-      -- adding any nvim-cmp sources here will enable them
-      -- with blink.compat
-      compat = {},
-      default = { "lsp", "path", "snippets", "buffer" },
-      per_filetype = {
-        codecompanion = { "codecompanion" },
-      },
-    },
-  },
-}
+if true then
+  return {}
+end
+
+-- return {
+--   "saghen/blink.cmp",
+--   -- build = "cargo build --release",
+--   ---@class PluginLspOpts
+--   opts = {
+--     fuzzy = { implementation = "prefer_rust" },
+--     signature = { enabled = true },
+--     keymap = {
+--       preset = "super-tab",
+--     },
+--     sources = {
+--       -- adding any nvim-cmp sources here will enable them
+--       -- with blink.compat
+--       compat = {},
+--       default = { "lsp", "path", "snippets", "buffer" },
+--       per_filetype = {
+--         codecompanion = { "codecompanion" },
+--       },
+--     },
+--   },
+-- }
